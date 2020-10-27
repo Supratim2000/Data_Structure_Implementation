fb769aba2638295a6e8922cda2374d13ca2dbcac
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2020-10-27 19:42:49 +0530

    Update Stack.cpp

--- Dynamic Stack/Stack.cpp
+++ Dynamic Stack/Stack.cpp
@@ -1,75 +1,76 @@
 #include <iostream>
-#include<climits>
+#include <climits>
 using namespace std;
 
+template<typename T>
 class Stack
 {
-    private:
-        int *data;
-        int nextIndex;
-        int capacity;
-        int defaultCapacity = 5;
+private:
+    T *data;
+    int nextIndex;
+    int capacity;
+    int defaultCapacity = 5;
 
-    public:
-        Stack()
-        {
-            capacity = defaultCapacity;
-            data = new int[capacity];
-            nextIndex = 0;
-        }
+public:
+    Stack()
+    {
+        capacity = defaultCapacity;
+        data = new T[capacity];
+        nextIndex = 0;
+    }
 
-        int Size()
-        {
-            return nextIndex;
-        }
+    int Size()
+    {
+        return nextIndex;
+    }
 
-        bool isEmpyt()
-        {
-            return nextIndex == 0;
-        }
+    bool isEmpyt()
+    {
+        return nextIndex == 0;
+    }
 
-        bool isFull()
-        {
-            return capacity == nextIndex;
-        }
+    bool isFull()
+    {
+        return capacity == nextIndex;
+    }
 
-        //To insert element in Stack
-        void push(int element)
+    //To insert element in Stack
+    void push(T element)
+    {
+        if (isFull())
         {
-            if(isFull())
-            {
-                int *newData = new int[capacity*2];
-                for(int i=0;i<capacity;i++)
-                    newData[i]=data[i]; 
-                capacity *= 2;
-                delete [] data;
-                data = newData;
-            }
-            data[nextIndex] = element;
-            nextIndex++;
+            T *newData = new T[capacity * 2];
+            for (int i = 0; i < capacity; i++)
+                newData[i] = data[i];
+            capacity *= 2;
+            delete[] data;
+            data = newData;
         }
+        data[nextIndex] = element;
+        nextIndex++;
+    }
 
-        //To remove element in Stack 
-        int pop()
+    //To remove element in Stack
+    int pop()
+    {
+        if (isEmpyt())
         {
-            if(isEmpyt())
-            {
-                cout<<"Stack is Empty"<<endl;
-                return INT_MIN;
-            }
-            int temp = data[nextIndex-1];
-            nextIndex--;
-            return temp;
+            cout << "Stack is Empty" << endl;
+            return 0;
         }
+        T temp = data[nextIndex - 1];
+        nextIndex--;
+        return temp;
+    }
 
-        //Reads the top most element in Stack
-        int top()
+    //Reads the top most element in Stack
+    int top()
+    {
+        if (isEmpyt())
         {
-            if(isEmpyt())
-            {
-                cout<<"Stack is empty"<<endl;
-                return INT_MIN;
-            }
-            return data[nextIndex-1];
+            cout << "Stack is empty" << endl;
+            return 0;
         }
+        return data[nextIndex - 1];
+    }
 };
